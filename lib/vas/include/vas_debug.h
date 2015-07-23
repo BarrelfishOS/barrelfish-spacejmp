a1aa739e520612a797e0fd728a8eac39ef49a31c
Reto Achermann <reto.achermann@inf.ethz.ch>
2015-07-23 15:46:43 +0200

    libvas: adding override for debug if NDEBUG is set
    Signed-off-by: Reto Achermann <reto.achermann@inf.ethz.ch>

--- lib/vas/include/vas_debug.h
+++ lib/vas/include/vas_debug.h
@@ -10,11 +10,15 @@
 #ifndef __VAS_DEBUG_H_
 #define __VAS_DEBUG_H_ 1
 
-
-#define VAS_DEBUG_GLOBAL_ENABLE 1
+#define VAS_DEBUG_GLOBAL_ENABLE 0
 #define VAS_DEBUG_VAS_ENABLE 1
 #define VAS_DEBUG_VSPACE_ENABLE 1
 #define VAS_DEBUG_CLIENT_ENABLE 1
+
+#ifdef NDEBUG
+#undef VAS_DEBUG_GLOBAL_ENABLE
+#define VAS_DEBUG_GLOBAL_ENABLE 0
+#endif
 
 #if VAS_DEBUG_GLOBAL_ENABLE
 #define VAS_DEBUG_PRINTF(x...) debug_printf("[vas] " x);
