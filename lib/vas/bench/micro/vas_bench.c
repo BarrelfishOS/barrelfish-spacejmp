d542dca3e5eb2e8d1ea0dcba1dca6355f32fb273
Reto Achermann <reto.achermann@inf.ethz.ch>
2015-08-11 16:53:01 +0200

    vasbench: adding segment detach
    Signed-off-by: Reto Achermann <reto.achermann@inf.ethz.ch>

--- lib/vas/bench/micro/vas_bench.c
+++ lib/vas/bench/micro/vas_bench.c
@@ -31,8 +31,16 @@
     if (!expr) {USER_PANIC(str);}
 
 //#define ITERATIONS 256ULL
-#define ITERATIONS 256ULL
-#define DRYRUNS 6
+#define ITERATIONS 512ULL
+#define DRYRUNS 16
+#define TOTAL_RUNS (ITERATIONS + DRYRUNS)
+
+
+#define SEGMENT_SIZE_MIN_BITS 12
+#define SEGMENT_SIZE_MAX_BITS 25
+
+
+#define SEGMENT_SIZE (4UL * HUGE_PAGE_SIZE)
 
 #if 0
 static void shuffle(size_t *array, size_t n) {
@@ -51,10 +59,12 @@ static void shuffle(size_t *array, size_t n) {
 #endif
 
 
+vas_handle_t vas[TOTAL_RUNS];
+vas_seg_handle_t seg[TOTAL_RUNS];
+
 static uint64_t micro_benchmarks(void) {
 
-    vas_handle_t vas[ITERATIONS];
-    vas_seg_handle_t seg[ITERATIONS];
+
     errval_t r;
 
 
@@ -97,7 +107,6 @@ static uint64_t micro_benchmarks(void) {
         bench_ctl_destroy(bench_ctl);
     }
 
-
     {
         bench_ctl_t *bench_ctl = bench_ctl_init(BENCH_MODE_FIXEDRUNS, 1, ITERATIONS);
         EXPECT_NONNULL(bench_ctl, "bench ctl was null");
@@ -107,7 +116,7 @@ static uint64_t micro_benchmarks(void) {
             cycles_t t_start = bench_tsc();
             r = vas_attach(vas[iter++], 0);
             cycles_t t_end = bench_tsc();
-            EXPECT_SUCCESS(r, "creating vas");
+            EXPECT_SUCCESS(r, "vas attach");
             t_elapsed = bench_time_diff(t_start, t_end);
         } while(!bench_ctl_add_run(bench_ctl, &t_elapsed));
 
@@ -115,24 +124,25 @@ static uint64_t micro_benchmarks(void) {
         bench_ctl_destroy(bench_ctl);
     }
 
-
     {
         struct capref frame;
-        r = frame_alloc(&frame, BASE_PAGE_SIZE, NULL);
+        r = frame_alloc(&frame, SEGMENT_SIZE, NULL);
         EXPECT_SUCCESS(r, "frame alloc");
 
         int iter = 0;
-        bench_ctl_t *bench_ctl = bench_ctl_init(BENCH_MODE_FIXEDRUNS, 1, ITERATIONS);
+        bench_ctl_t *bench_ctl = bench_ctl_init(BENCH_MODE_FIXEDRUNS, 1, TOTAL_RUNS);
         EXPECT_NONNULL(bench_ctl, "bench ctl was null");
 
         bench_ctl_dry_runs(bench_ctl, DRYRUNS);
         do {
-            lvaddr_t addr = (VAS_SEG_VADDR_MIN + iter * BASE_PAGE_SIZE);
+            lvaddr_t addr = (VAS_SEG_VADDR_MIN + iter * SEGMENT_SIZE);
             char str[10];
             snprintf(str, 10, "/seg/%u", iter);
             cycles_t t_start = bench_tsc();
-            r = vas_seg_alloc(str, VAS_SEG_TYPE_FIXED, BASE_PAGE_SIZE, addr,
-                              VREGION_FLAGS_READ_WRITE, &seg[iter++]);
+
+            r =  vas_seg_create(str, VAS_SEG_TYPE_FIXED, SEGMENT_SIZE,
+                               addr, frame, VREGION_FLAGS_READ_WRITE,
+                               &seg[iter++]);
             cycles_t t_end = bench_tsc();
             EXPECT_SUCCESS(r, "mapping in  vas");
             t_elapsed = bench_time_diff(t_start, t_end);
@@ -167,12 +177,8 @@ static uint64_t micro_benchmarks(void) {
 
 
     {
-        struct capref frame;
-        r = frame_alloc(&frame, BASE_PAGE_SIZE, NULL);
-        EXPECT_SUCCESS(r, "frame alloc");
-
         int iter = 0;
-        bench_ctl_t *bench_ctl = bench_ctl_init(BENCH_MODE_FIXEDRUNS, 1, ITERATIONS);
+        bench_ctl_t *bench_ctl = bench_ctl_init(BENCH_MODE_FIXEDRUNS, 1, TOTAL_RUNS);
         EXPECT_NONNULL(bench_ctl, "bench ctl was null");
 
         bench_ctl_dry_runs(bench_ctl, DRYRUNS);
@@ -192,11 +198,10 @@ static uint64_t micro_benchmarks(void) {
         bench_ctl_t *bench_ctl = bench_ctl_init(BENCH_MODE_FIXEDRUNS, 2, ITERATIONS);
         EXPECT_NONNULL(bench_ctl, "bench ctl was null");
         bench_ctl_dry_runs(bench_ctl, DRYRUNS);
-        int iter = 0;
         cycles_t t_elapsed2[2];
         do {
             cycles_t t_start = bench_tsc();
-            r = vas_switch(vas[iter++]);
+            r = vas_switch(vas[0]);
             cycles_t t_end = bench_tsc();
             EXPECT_SUCCESS(r, "creating vas");
             t_elapsed2[0] = bench_time_diff(t_start, t_end);
@@ -261,6 +266,108 @@ static uint64_t micro_benchmarks(void) {
 
 }
 
+
+
+static uint64_t seg_attach_detach(void) {
+
+
+    errval_t r;
+
+
+
+    cycles_t t_elapsed;
+
+    {
+        bench_ctl_t *bench_ctl = bench_ctl_init(BENCH_MODE_FIXEDRUNS, 1, TOTAL_RUNS);
+        EXPECT_NONNULL(bench_ctl, "bench ctl was null");
+        bench_ctl_dry_runs(bench_ctl, DRYRUNS);
+        int iter = 0;
+        do {
+            char str[10];
+            snprintf(str, 10, "/vas/%u", iter);
+            cycles_t t_start = bench_tsc();
+            r = vas_create(str, 0, &vas[iter++]);
+            cycles_t t_end = bench_tsc();
+            EXPECT_SUCCESS(r, "creating vas");
+            t_elapsed = bench_time_diff(t_start, t_end);
+        } while(!bench_ctl_add_run(bench_ctl, &t_elapsed));
+        bench_ctl_dump_analysis(bench_ctl, 0, "vas_create", bench_tsc_per_us());
+        bench_ctl_destroy(bench_ctl);
+    }
+
+
+    struct capref frame;
+    r = frame_alloc(&frame, (1UL << SEGMENT_SIZE_MAX_BITS), NULL);
+    EXPECT_SUCCESS(r, "frame alloc");
+
+
+    for (uint8_t bits = SEGMENT_SIZE_MIN_BITS; bits <= SEGMENT_SIZE_MAX_BITS; ++bits) {
+        char str[10];
+        snprintf(str, 10, "/seg/%u", bits);
+        r =  vas_seg_create(str, VAS_SEG_TYPE_FIXED, (1UL << bits),
+                            VAS_SEG_VADDR_MIN, frame, VREGION_FLAGS_READ_WRITE,
+                            &seg[bits]);
+        EXPECT_SUCCESS(r, "create segment");
+    }
+
+    r = vas_attach(vas[0], 0);
+        EXPECT_SUCCESS(r, "creating vas");
+
+    r = vas_switch(vas[0]);
+    EXPECT_SUCCESS(r, "creating vas");
+
+    for (uint8_t bits = SEGMENT_SIZE_MIN_BITS; bits <= SEGMENT_SIZE_MAX_BITS; ++bits) {
+        bench_ctl_t *bench_ctl_m = bench_ctl_init(BENCH_MODE_FIXEDRUNS, 1, TOTAL_RUNS);
+        bench_ctl_t *bench_ctl_u = bench_ctl_init(BENCH_MODE_FIXEDRUNS, 1, TOTAL_RUNS);
+
+        EXPECT_NONNULL(bench_ctl_m, "bench ctl was null");
+        EXPECT_NONNULL(bench_ctl_u, "bench ctl was null");
+
+        bench_ctl_dry_runs(bench_ctl_m, DRYRUNS);
+        bench_ctl_dry_runs(bench_ctl_u, DRYRUNS);
+        do {
+            cycles_t t_start = bench_tsc();
+            r = vas_seg_attach(vas[0], seg[bits], 0);
+            cycles_t t_end = bench_tsc();
+            EXPECT_SUCCESS(r, "mapping in  vas");
+            t_elapsed = bench_time_diff(t_start, t_end);
+            bench_ctl_add_run(bench_ctl_m, &t_elapsed);
+
+            uint64_t *ptr = (uint64_t *)( VAS_SEG_VADDR_MIN + (1UL << (bits -1)));
+            *ptr = 0xcafebabe;
+
+            t_start = bench_tsc();
+            r = vas_seg_detach(vas[0], seg[bits]);
+            t_end = bench_tsc();
+            EXPECT_SUCCESS(r, "mapping in  vas");
+            t_elapsed = bench_time_diff(t_start, t_end);
+        } while(!bench_ctl_add_run(bench_ctl_u, &t_elapsed));
+
+        char label[30];
+        snprintf(label, 30, "seg_attach(%u)", bits);
+
+        bench_ctl_dump_analysis(bench_ctl_m, 0, label, bench_tsc_per_us());
+        snprintf(label, 30, "seg_detach(%u)", bits);
+        bench_ctl_dump_analysis(bench_ctl_u, 0, label, bench_tsc_per_us());
+        bench_ctl_destroy(bench_ctl_m);
+        bench_ctl_destroy(bench_ctl_u);
+    }
+
+
+    debug_printf("TSC per MS: %lu\n", bench_tsc_per_ms());
+
+
+    r = vas_tagging_enable();
+    EXPECT_SUCCESS(r, "tagging enable");
+
+    //CHECK(r, "disable tags", vas_tagging_disable());
+
+    printf("microbenchmarks done\n");
+
+    return 0;
+
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -269,7 +376,13 @@ int main(int argc, char *argv[])
     vas_enable();
 
 
-    micro_benchmarks();
+    if (0) {
+        micro_benchmarks();
+    }
+    seg_attach_detach();
+
+    while(1)
+        ;
 
     return 0;
 }
